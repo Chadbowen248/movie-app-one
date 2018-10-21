67915068efee6c9ed01e905d5c1a296afdecfb32
Impactionplatform <Chadbowen248@gmail.com>
2018-10-21 12:39:14 -0400

    setting up deploy

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const API_KEY = 'b214b2f6cd4cb56d9c0a986a2215d33f'
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+
 app.get('/search/:movie', (req, res) => {
   const movie = req.params.movie;
   axios(`http://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${movie}`)
@@ -16,5 +18,14 @@ app.get('/search/:movie', (req, res) => {
   })))
   .then(data => res.json(data))
 })
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
